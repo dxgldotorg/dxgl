f9ddc70e9b5c94483cf878e6773e79afbc000c94
admin <admin@8a90861a-4eca-46d5-b744-240ff16d0c4d>
2015-02-08 20:44:34 +0000

    Sync before reading renderer caps.
    git-svn-id: https://www.williamfeely.info/svn/dxgl@593 8a90861a-4eca-46d5-b744-240ff16d0c4d

--- ddraw/glDirectDraw.cpp
+++ ddraw/glDirectDraw.cpp
@@ -1164,6 +1164,7 @@ HRESULT WINAPI glDirectDraw7::GetCaps(LPDDCAPS lpDDDriverCaps, LPDDCAPS lpDDHELC
 		mode.dmSize = sizeof(DEVMODE);
 		EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &mode);
 		glRenderer_Init(tmprenderer, 16, 16, mode.dmBitsPerPel, false, mode.dmDisplayFrequency, hGLWnd, NULL, FALSE);
+		glRenderer_Sync(tmprenderer);
 		if (tmprenderer->ext->glver_major >= 3) fullrop = TRUE;
 		if (tmprenderer->ext->GLEXT_EXT_gpu_shader4) fullrop = TRUE;
 		glRenderer_Delete(tmprenderer);
@@ -1171,6 +1172,7 @@ HRESULT WINAPI glDirectDraw7::GetCaps(LPDDCAPS lpDDDriverCaps, LPDDCAPS lpDDHELC
 	}
 	else
 	{
+		glRenderer_Sync(renderer);
 		if (renderer->ext->glver_major >= 3) fullrop = TRUE;
 		if (renderer->ext->GLEXT_EXT_gpu_shader4) fullrop = TRUE;
 	}
