4f746af4eafe850bd733a8dbcf5d4df53fb26b5f
William Feely <william@dxgl.org>
2020-09-07 15:25:08 -0400

    Make builtin shaders OpenGL 3+ compatible.

--- ddraw/ShaderManager.cpp
+++ ddraw/ShaderManager.cpp
@@ -27,16 +27,37 @@
 
 extern "C" {
 
+static const char version_110[] = "#version 110\n";
+static const char version_120[] = "#version 120\n";
+static const char version_130[] = "#version 130\n";
+static const char version_140[] = "#version 140\n";
+static const char version_150[] = "#version 150\n";
+static const char version_330[] = "#version 330\n";
+static const char version_400[] = "#version 400\n";
+static const char version_410[] = "#version 410\n";
+static const char version_420[] = "#version 420\n";
+static const char version_430[] = "#version 430\n";
+static const char version_440[] = "#version 440\n";
+static const char version_450[] = "#version 450\n";
+static const char version_460[] = "#version 460\n";
+	
 const char frag_Texture[] = "\
-#version 110\n\
 uniform sampler2D tex0;\n\
 void main()\n\
 {\n\
 	gl_FragColor = texture2D( tex0, gl_TexCoord[0].st);\n\
 }";
 
+const char frag_Texture_gl3[] = "\
+uniform sampler2D tex0;\n\
+in vec4 TexCoord0;\n\
+out vec4 FragColor;\n\
+void main()\n\
+{\n\
+	FragColor = texture2D( tex0, TexCoord0.st);\n\
+}";
+
 const char frag_Pal256[] =  "\
-#version 110\n\
 uniform sampler2D pal;\n\
 uniform sampler2D tex0;\n\
 void main()\n\
@@ -47,15 +68,33 @@ void main()\n\
 	gl_FragColor = texel;\n\
 }";
 
+const char frag_Pal256_gl3[] = "\
+uniform sampler2D pal;\n\
+uniform sampler2D tex0;\n\
+in vec4 TexCoord0;\n\
+out vec4 FragColor;\n\
+void main()\n\
+{\n\
+	vec4 myindex = texture2D(tex0, TexCoord0.xy);\n\
+	vec2 index = vec2(((myindex.x*(255.0/256.0))+(0.5/256.0)),0.5);\n\
+	vec4 texel = texture2D(pal, index);\n\
+	FragColor = texel;\n\
+}";
+
 const char frag_clipstencil[] = "\
-#version 110\n\
 void main ()\n\
 {\n\
 	gl_FragColor = vec4(1.0,0.0,0.0,0.0);\n\
 }";
 
+const char frag_clipstencil_gl3[] = "\
+out vec4 FragColor;\n\
+void main ()\n\
+{\n\
+	FragColor = vec4(1.0,0.0,0.0,0.0);\n\
+}";
+
 const char frag_prepsrckey[] = "\
-#version 110\n\
 uniform sampler2D tex0;\n\
 uniform ivec3 ckey;\n\
 uniform ivec4 colorsize;\n\
@@ -68,8 +107,22 @@ void main()\n\
 	gl_FragColor = vec4(pixel) / vec4(colorsize);\n\
 }";
 
+const char frag_prepsrckey_gl3[] = "\
+uniform sampler2D tex0;\n\
+uniform ivec3 ckey;\n\
+uniform ivec4 colorsize;\n\
+in vec4 TexCoord0;\n\
+ivec4 pixel;\n\
+out vec4 FragColor;\n\
+void main()\n\
+{\n\
+	pixel = ivec4(texture2D(tex0, TexCoord0.st)*vec4(colorsize) + .5);\n\
+	if (pixel.rgb == ckey) pixel.a = 0;\n\
+	else pixel.a = colorsize.a;\n\
+	FragColor = vec4(pixel) / vec4(colorsize);\n\
+}";
+
 const char frag_prepdestkey[] = "\
-#version 110\n\
 uniform sampler2D tex0;\n\
 uniform ivec4 colorsize;\n\
 uniform ivec3 ckey;\n\
@@ -82,8 +135,22 @@ void main()\n\
 	gl_FragColor = vec4(pixel) / vec4(colorsize);\n\
 }";
 
+const char frag_prepdestkey_gl3[] = "\
+uniform sampler2D tex0;\n\
+uniform ivec4 colorsize;\n\
+uniform ivec3 ckey;\n\
+in vec4 TexCoord0;\n\
+ivec4 pixel;\n\
+out vec4 FragColor;\n\
+void main()\n\
+{\n\
+	pixel = ivec4(texture2D(tex0, TexCoord0.st)*vec4(colorsize) + .5);\n\
+	if (pixel.rgb != ckey) pixel.a = 0;\n\
+	else pixel.a = colorsize.a;\n\
+	FragColor = vec4(pixel) / vec4(colorsize);\n\
+}";
+
 const char vert_ortho[] = "\
-#version 110\n\
 uniform vec4 view;\n\
 attribute vec2 xy;\n\
 attribute vec2 st;\n\
@@ -100,6 +167,23 @@ void main()\n\
 	gl_TexCoord[0] = vec4(st,0.0,1.0);\n\
 } ";
 
+const char vert_ortho_gl3[] = "\
+uniform vec4 view;\n\
+in vec2 xy;\n\
+in vec2 st;\n\
+out vec4 TexCoord0;\n\
+void main()\n\
+{\n\
+	vec4 xyzw = vec4(xy[0],xy[1],0,1);\n\
+	mat4 proj = mat4(\n\
+    vec4(2.0 / (view[1] - view[0]), 0, 0, 0),\n\
+    vec4(0, 2.0 / (view[2] - view[3]), 0, 0),\n\
+    vec4(0, 0, -2.0, 0),\n\
+    vec4(-(view[1] + view[0]) / (view[1] - view[0]),\n\
+ -(view[2] + view[3]) / (view[2] - view[3]), -1 , 1));\n\
+	gl_Position    = proj * xyzw;\n\
+	TexCoord0 = vec4(st,0.0,1.0);\n\
+} ";
 
 
 // Use EXACTLY one line per entry.  Don't change layout of the list.
@@ -115,13 +199,92 @@ const SHADER shader_template[] =
 const int SHADER_END = __LINE__ - 4;
 const int NumberOfShaders = SHADER_END - SHADER_START;
 
+const SHADER shader_template_gl3[] = 
+{
+	{0,0,	vert_ortho_gl3,		frag_Texture_gl3,	0,-1,-1,-1,-1,-1,-1,-1,-1},
+	{0,0,	vert_ortho_gl3,		frag_Pal256_gl3,	0,-1,-1,-1,-1,-1,-1,-1,-1},
+	{0,0,	vert_ortho_gl3,		frag_clipstencil_gl3,0,-1,-1,-1,-1,-1,-1,-1,-1},
+	{0,0,	vert_ortho_gl3,		frag_prepsrckey_gl3,0,-1,-1,-1,-1,-1,-1,-1,-1},
+	{0,0,	vert_ortho_gl3,		frag_prepdestkey_gl3,0,-1,-1,-1,-1,-1,-1,-1,-1}
+};
+
+
+void glslver(STRING* str, int major, int minor)
+{
+	switch (major)
+	{
+	case 2:
+		switch (minor)
+		{
+		case 0:
+			String_Assign(str, version_110);
+			break;
+		case 1:
+		default:
+			String_Assign(str, version_120);
+		}
+		break;
+	case 3:
+		switch (minor)
+		{
+		case 0:
+			String_Assign(str, version_130);
+			break;
+		case 1:
+			String_Assign(str, version_140);
+			break;
+		case 2:
+			String_Assign(str, version_150);
+			break;
+		case 3:
+		default:
+			String_Assign(str, version_330);
+			break;
+		}
+		break;
+	case 4:
+		switch (minor)
+		{
+		case 0:
+			String_Assign(str, version_400);
+			break;
+		case 1:
+			String_Assign(str, version_410);
+			break;
+		case 2:
+			String_Assign(str, version_420);
+			break;
+		case 3:
+			String_Assign(str, version_430);
+			break;
+		case 4:
+			String_Assign(str, version_440);
+			break;
+		case 5:
+			String_Assign(str, version_450);
+			break;
+		case 6:
+		default:
+			String_Assign(str, version_460);
+			break;
+		}
+		break;
+	default:
+		if (major > 4) String_Assign(str, version_460);
+		else String_Assign(str, version_110);
+		break;
+	}
+}
 
 void ShaderManager_Init(glExtensions *glext, ShaderManager *shaderman)
 {
 	shaderman->ext = glext;
 	shaderman->shaders = (SHADER*)malloc(sizeof(SHADER)*NumberOfShaders);
-	memcpy(shaderman->shaders, shader_template, sizeof(SHADER)*NumberOfShaders);
-	const GLchar *src;
+	if (glext->glver_major >= 3)
+		memcpy(shaderman->shaders, shader_template_gl3, sizeof(SHADER) * NumberOfShaders);
+	else memcpy(shaderman->shaders, shader_template, sizeof(SHADER) * NumberOfShaders);
+	STRING src;
+	ZeroMemory(&src, sizeof(STRING));
 	GLint srclen;
 	for(int i = 0; i < NumberOfShaders; i++)
 	{
@@ -129,18 +292,20 @@ void ShaderManager_Init(glExtensions *glext, ShaderManager *shaderman)
 		if(shaderman->shaders[i].vsrc)
 		{
 			shaderman->shaders[i].vs = shaderman->ext->glCreateShader(GL_VERTEX_SHADER);
-			src = shaderman->shaders[i].vsrc;
-			srclen = strlen(shaderman->shaders[i].vsrc);
-			shaderman->ext->glShaderSource(shaderman->shaders[i].vs,1,&src,&srclen);
+			glslver(&src, glext->glver_major, glext->glver_minor);
+			String_Append(&src,shaderman->shaders[i].vsrc);
+			srclen = strlen(src.ptr);
+			shaderman->ext->glShaderSource(shaderman->shaders[i].vs,1,&src.ptr,&srclen);
 			shaderman->ext->glCompileShader(shaderman->shaders[i].vs);
 			shaderman->ext->glAttachShader(shaderman->shaders[i].prog,shaderman->shaders[i].vs);
 		}
 		if(shaderman->shaders[i].fsrc)
 		{
 			shaderman->shaders[i].fs = shaderman->ext->glCreateShader(GL_FRAGMENT_SHADER);
-			src = shaderman->shaders[i].fsrc;
-			srclen = strlen(shaderman->shaders[i].fsrc);
-			shaderman->ext->glShaderSource(shaderman->shaders[i].fs,1,&src,&srclen);
+			glslver(&src, glext->glver_major, glext->glver_minor);
+			String_Append(&src, shaderman->shaders[i].fsrc);
+			srclen = strlen(src.ptr);
+			shaderman->ext->glShaderSource(shaderman->shaders[i].fs,1,&src.ptr,&srclen);
 			shaderman->ext->glCompileShader(shaderman->shaders[i].fs);
 			shaderman->ext->glAttachShader(shaderman->shaders[i].prog,shaderman->shaders[i].fs);
 		}
