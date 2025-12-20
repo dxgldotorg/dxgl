6bdb1449985b7cf6a0d8fe87b973df3662eba8db
dxgldotorg <william@dxgl.org>
2025-12-19 23:57:03 -0500

    Update redistributable to Visual C++ 2026. Change projects to use Visual C++ 2026 instead of 2022. Update system requirements for VS2026 build to Windows 10.

--- buildtool/buildtool.c
+++ buildtool/buildtool.c
@@ -523,13 +523,14 @@ int ProcessHeaders(char *path)
 			strncpy(findptr, "\"VC2010\"\n", 13);
 			#elif (_MSC_VER == 1800)
 			strncpy(findptr, "\"VC2013\"\n", 13);
-			#elif (_MSC_VER == 1944)
-			strncpy(findptr, "\"VC2022_14\"\n", 13);
-			#elif ((_MSC_VER >= 1930) && (_MSC_VER < 1944))
-			#error Please update your Visual Studio 2022 to Update 14 before continuing.  If you have an expired MSDN subscription and cannot update your paid version of Visual Studio, you can still use the Community version to compile DXGL.
-			#elif (_MSC_VER > 1943)
-			#pragma message ("Detected a newer version of Visual Studio, compiling assuming 2022.14.")
-			strncpy(findptr, "\"VC2022_14\"\n", 13);
+			#elif (_MSC_VER == 1950)
+			strncpy(findptr, "\"VC2026_0\"\n", 13);
+			//#elif ((_MSC_VER >= 1950) && (_MSC_VER < 1950))
+			//#error Please update your Visual Studio 2026 to Update 0 before continuing.  If you have an expired MSDN subscription and cannot update your paid version of Visual Studio, you can still use the Community version to compile DXGL.
+			//#error If this error persists after updating Visual Studio, try uninstalling older MSVC toolchains.*/
+			#elif (_MSC_VER > 1950)
+			#pragma message ("Detected a newer version of Visual Studio, compiling assuming 2026.0.")
+			strncpy(findptr, "\"VC2026_0\"\n", 13);
 			#else
 			#pragma message ("Can't detect MSVC version!")
 			strncpy(findptr, "\"UNKNOWN\"\n", 13);
