45ce0ff268655777b6d34e8b99ac5dc22e7cd1a4
dxgldotorg <william@dxgl.org>
2026-03-07 01:44:07 -0500

    Set DDSCAPS_LOCALVIDMEM if not specified for video memory surfaces. Validate against combo of DDSCAPS_SYSTEMMEMORY | DDSCAPS_VIDEOMEMORY and DDSCAPS_LOCALVIDMEM | DDSCAPS_NONLOCALVIDMEM

--- ddraw/dxglDirectDrawSurface.cpp
+++ ddraw/dxglDirectDrawSurface.cpp
@@ -336,10 +336,22 @@ HRESULT dxglDirectDrawSurface7_Create(LPDIRECTDRAW7 lpDD7, LPDDSURFACEDESC2 lpDD
 	}
 	else if (!((glDDS7->ddsd.dwFlags & DDSD_WIDTH) && (glDDS7->ddsd.dwFlags & DDSD_HEIGHT)))
 	TRACE_RET(HRESULT, 23, DDERR_INVALIDPARAMS);
-
+	if ((glDDS7->ddsd.ddsCaps.dwCaps & DDSCAPS_SYSTEMMEMORY) && (glDDS7->ddsd.ddsCaps.dwCaps & DDSCAPS_VIDEOMEMORY))
+		TRACE_RET(HRESULT, 23, DDERR_INVALIDCAPS);
 	if (!(glDDS7->ddsd.ddsCaps.dwCaps & DDSCAPS_SYSTEMMEMORY))
 		glDDS7->ddsd.ddsCaps.dwCaps |= DDSCAPS_VIDEOMEMORY;
-
+	if (glDDS7->ddsd.ddsCaps.dwCaps & DDSCAPS_SYSTEMMEMORY)
+	{
+		if (glDDS7->ddsd.ddsCaps.dwCaps & (DDSCAPS_LOCALVIDMEM | DDSCAPS_NONLOCALVIDMEM))
+			TRACE_RET(HRESULT, 23, DDERR_INVALIDCAPS);
+	}
+	if((glDDS7->ddsd.ddsCaps.dwCaps & DDSCAPS_LOCALVIDMEM) && (glDDS7->ddsd.ddsCaps.dwCaps & DDSCAPS_NONLOCALVIDMEM))
+		TRACE_RET(HRESULT, 23, DDERR_INVALIDCAPS);
+	if (glDDS7->ddsd.ddsCaps.dwCaps & DDSCAPS_VIDEOMEMORY)
+	{
+		if (!(glDDS7->ddsd.ddsCaps.dwCaps & (DDSCAPS_LOCALVIDMEM | DDSCAPS_NONLOCALVIDMEM)))
+			glDDS7->ddsd.ddsCaps.dwCaps |= DDSCAPS_LOCALVIDMEM;
+	}
 	// Not yet implemented: System memory surface
 	/*	if(glDDS7->ddsd.ddsCaps.dwCaps & DDSCAPS_SYSTEMMEMORY)
 	{
